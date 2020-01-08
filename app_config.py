13a94e9e2f7dcdafbb65bf1d64ea891d11d5606a
Akshay <akshay@Akshays-MacBook-Pro.local>
2020-01-08 11:48:30 -0700

    add data

--- app_config.py
+++ app_config.py
@@ -19,7 +19,7 @@ param_mixes = [
 ]
 
 clusterone_params = ['0.2', '0.5', '0.8', '0.9', '0.95', '0.99', '0.8_batch_corrected', '0.9_batch_corrected', '0.95_batch_corrected', '0.99_batch_corrected']
-params['plot_data_df_path'] = param_mixes + [data_pfx + "data/vizdf_GLS01_CO99_d_{}.csv".format(x) for x in clusterone_params]
+params['plot_data_df_path'] = [param_mixes[0]] #+ [data_pfx + "data/vizdf_GLS01_CO99_d_{}.csv".format(x) for x in clusterone_params]
 params['raw_ess_data_path'] = "{}data/essentiality.tsv.gz".format(data_pfx)
 params['mutation_data_path'] = "{}data/CCLE_DepMap_18q3_maf_20180718.txt".format(data_pfx)
 params['mutation_arr_path'] = "{}data/CCLE_DepMap_18q3_mutations.npy".format(data_pfx)
