c6260334f28e5ca06107dcf118d774c24cc0f4cf
anna shcherbina <annashcherbina@gmail.com>
2020-06-29 21:55:42 -0700

    back to app.server

--- app.py
+++ app.py
@@ -769,6 +769,6 @@ def update_landscape(
 # =======================================================
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(server,host='0.0.0.0',port=5000)
-    #app.run_server(port=8052, debug=True)
+    #from waitress import serve
+    #serve(server,host='0.0.0.0',port=5000)
+    app.run_server(host='0.0.0.0', port=5000, debug=True)
