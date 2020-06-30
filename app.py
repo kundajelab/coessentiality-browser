99f8a32b592089d08b41e8b3ff99a26ba5c552fd
anna shcherbina <annashcherbina@gmail.com>
2020-06-29 21:40:53 -0700

    changing waitress syntax

--- app.py
+++ app.py
@@ -770,5 +770,5 @@ def update_landscape(
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app,host='0.0.0.0',port=5000)
+    serve(server,host='0.0.0.0',port=5000)
     #app.run_server(port=8052, debug=True)
